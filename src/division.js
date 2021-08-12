4c2c70fbf02df837a5b4252af9ae78c37690594f
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-08-12 10:57:10 -0300

    importexport

---
+++ src/division.js
@@ -0,0 +1,3 @@
+export default (a, b) => a / b
+
+export const multiplication = (a, b) => a * b
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+// import * as calculator from './calculator'
+import { subtraction, sum } from './calculator'
+
+import div, { multiplication } from './division'
+
+console.log(subtraction(10, 3))
+console.log(sum(10, 3))
+
+console.log(div(5, 5))
+console.log(multiplication(5, 2))
